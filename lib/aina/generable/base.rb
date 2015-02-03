edeee82fc16e684cade09c75e02171c79f83ccd9
Carles Jove i Buxeda <hola@carlus.cat>
2015-02-03 11:48:05 +0100

    retab

--- lib/aina/generable/base.rb
+++ lib/aina/generable/base.rb
@@ -1,72 +1,72 @@
 # Generable class
 class Generable::Base
-	extend Generable
-	attr_reader :name, :template, :file, :aina_version, :supports
+  extend Generable
+  attr_reader :name, :template, :file, :aina_version, :supports
 
-	def initialize(name, options=nil)
-		@name = name
-		@options = options
-		@aina_version = Aina::VERSION
-		@template = self.template
-		@dir = self.dir
-		@file = generate_file_name
+  def initialize(name, options=nil)
+    @name = name
+    @options = options
+    @aina_version = Aina::VERSION
+    @template = self.template
+    @dir = self.dir
+    @file = generate_file_name
 
-		set_custom_vars
-	end
-	
-	def set_custom_vars
-		# Nothing here
-		# This can be used in types to parse required instance variables
-	end
+    set_custom_vars
+  end
 
-	def dir
-		self.class.get_dir
-	end
+  def set_custom_vars
+    # Nothing here
+    # This can be used in types to parse required instance variables
+  end
 
-	def template
-		self.class.get_template
-	end
+  def dir
+    self.class.get_dir
+  end
 
-	def replacements
-		self.class.get_replacements
-	end
+  def template
+    self.class.get_template
+  end
 
-	def generate
-		text = File.read(@template)
-		replacements.each do |replace|
-			attribute = replace.gsub(/[{}]/, '')
-			@output = text.gsub!(/#{replace}/, self.send(attribute))
-		end
-  	File.open(@file, "w") {|file| file.puts @output}
+  def replacements
+    self.class.get_replacements
+  end
 
-  	after_generate
-	end
+  def generate
+    text = File.read(@template)
+    replacements.each do |replace|
+      attribute = replace.gsub(/[{}]/, '')
+      @output = text.gsub!(/#{replace}/, self.send(attribute))
+    end
+    File.open(@file, "w") {|file| file.puts @output}
 
-	def after_generate
-		if self.class.get_after_generate
-			self.class.get_after_generate.each do |callback|
-				self.send(callback)
-			end
-		end
-	end
+    after_generate
+  end
 
-	def destroy
-		if File.exists?(@file)
-			File.delete(@file)
-		else
-			raise "No #{self.class} with name #{@name}"
-		end
-	end
+  def after_generate
+    if self.class.get_after_generate
+      self.class.get_after_generate.each do |callback|
+        self.send(callback)
+      end
+    end
+  end
 
-	private
-		def generate_file_name
-			unless @dir.nil?
-				unless Dir.exists?(Dir.pwd + '/' + @dir)
-					Dir.mkdir(Dir.pwd + '/' + @dir)
-				end
-				Dir.pwd + "/#{@dir}/#{@name}.php"
-			else
-				Dir.pwd + "/#{@name}.php"
-			end
-		end
+  def destroy
+    if File.exists?(@file)
+      File.delete(@file)
+    else
+      raise "No #{self.class} with name #{@name}"
+    end
+  end
+
+  private
+    def generate_file_name
+      unless @dir.nil?
+        unless Dir.exists?(Dir.pwd + '/' + @dir)
+          Dir.mkdir(Dir.pwd + '/' + @dir)
+        end
+        Dir.pwd + "/#{@dir}/#{@name}.php"
+      else
+        Dir.pwd + "/#{@name}.php"
+      end
+    end
 end
